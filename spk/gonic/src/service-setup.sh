40412469aad7abe4f341b73153f70b9145c5b1c1
Peter van Zetten <peter.vanzetten@gmail.com>
2020-05-05 11:38:08 +0100

    Add gonic music streaming server

---
+++ spk/gonic/src/service-setup.sh
@@ -0,0 +1,30 @@
+# Setup environment
+PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
+GROUP="sc-gonic"
+
+INST_ETC="/var/packages/${SYNOPKG_PKGNAME}/etc"
+INST_VARIABLES="${INST_ETC}/installer-variables"
+
+service_postinst ()
+{
+    echo "Running service_postinst script" >> "${INST_LOG}"
+    
+    echo "WIZARD_MUSIC_PATH=${wizard_music_path}" >> ${INST_VARIABLES}
+
+    echo "Install busybox binaries" >> "${INST_LOG}"
+    BIN=${SYNOPKG_PKGDEST}/bin
+    $BIN/busybox --install $BIN >> ${INST_LOG}
+}
+
+service_prestart ()
+{
+    if [ -f ${INST_VARIABLES} ]
+    then
+      . ${INST_VARIABLES}
+    fi
+
+	SERVICE_OPTIONS="-music-path ${WIZARD_MUSIC_PATH} -db-path ${SYNOPKG_PKGDEST}/var/gonic.db"
+    # Required: start-stop-daemon do not set environment variables
+    HOME=${SYNOPKG_PKGDEST}/var
+    export HOME
+}
